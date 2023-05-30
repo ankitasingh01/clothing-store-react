5578e955e818de8a39b4b022872910793362080a
“AnkitaSingh” <er.ankita9@gmail.com>
2023-05-30 13:58:44 +0530

    Adding Redirects for netlify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import NavigationBar from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
